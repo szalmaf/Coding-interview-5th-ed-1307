6633fee2ce94c68ea5b89f2dcb10dd84a6cd53f2
Ferenc Szalma <szalmaf@users.noreply.github.com>
2016-06-30 14:53:07 -0400

    Working code.

--- Codeing Interview 1307/main.cpp
+++ Codeing Interview 1307/main.cpp
@@ -29,28 +29,50 @@ Node* meth(Node* p)
     return newp;
 }
 
+typedef map<Node*, Node*> mapNN;
 
-Node* copy_nodes(Node* nodep){
-    return nodep;
+// Recursive function to copy node structure starting from current node
+Node* copy_nodes(Node* nodep, mapNN & nodemap){
+    if(nodep == NULL)
+        return NULL;
+    
+    mapNN::iterator mapNNi = nodemap.find(nodep);
+    if(mapNNi != nodemap.end())
+        return mapNNi->second;
+    
+    Node* newnode = new Node();
+    nodemap[nodep] = newnode; // Add new entry to map with old and new (copy) pointer
+    newnode->ptr1 = copy_nodes(nodep->ptr1, nodemap);
+    newnode->ptr2 = copy_nodes(nodep->ptr2, nodemap);
+    
+    return newnode;
 }
 
-
+// nodemap's two columns contain the old and the new, copied node pointers
+// Node's are copied by recursively calling the copy_nodes function
 Node* create_copy(Node* rootp){
-    map<Node*, Node*> nodemap;
-    Node* new_rootp = copy_nodes(rootp);
+    mapNN nodemap;
+    Node* new_rootp = copy_nodes(rootp, nodemap);
     return new_rootp;
 }
 
 
-// Code to copy a complete node structure or at least the pointers to the nodes
+// Code to copy a complete node structure or at least the pointers pointing at the nodes
 int main (int argc, char *argv[])
 {
-    Node *o = new Node(new Node(NULL, new Node()), new Node());
+    // Example node structure.
+    Node *o = new Node(
+                       new Node(
+                                NULL,
+                                new Node()),
+                       new Node());
+    
+    // Create a copy of the node structure.
     Node *n = create_copy(o);
     
     
     
-    
+    // Stop before ending the program.
     int a;
     cin >> a;
     
