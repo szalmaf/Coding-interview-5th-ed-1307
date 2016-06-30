2807e200f25788e294f7f4762545db438e4a4005
Ferenc Szalma <szalmaf@users.noreply.github.com>
2016-06-30 12:35:48 -0400

    Initial Commit

---
+++ Codeing Interview 1307/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Codeing Interview 1307
+//
+//  Created by Ferenc Szalma on 6/30/16.
+//  Copyright © 2016 Ferenc Szalma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
